fa62bacb6469c6c05a9f4247176940f9805aa466
Krzysztof Stanek <ekstanek@gmail.com>
2025-03-01 00:46:34 +0100

    dynamic theme

--- resources/views/partial/head.blade.php
+++ resources/views/partial/head.blade.php
@@ -4,35 +4,35 @@
     <link rel="search" type="application/opensearchdescription+xml"
         href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}out/out.OpensearchDesc.php" title="SeedDMS" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
-    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/styles/seeddms.css">
+    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/styles/seeddms.css">
     <link rel="stylesheet"
-        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/font-awesome/css/font-awesome.min.css">
+        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet"
-        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/bootstrap-datepicker/css/bootstrap-datepicker.css">
+        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/bootstrap-datepicker/css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}styles/bootstrap/chosen/css/chosen.css">
     <link rel="stylesheet"
-        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/select2/css/select2.min.css">
+        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/select2/css/select2.min.css">
     <link rel="stylesheet"
-        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/jqtree/jqtree.css">
-    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/styles/application.css">
-    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/styles/styles.css">
+        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/select2-{{ $user->theme }}-theme/select2-{{ $user->theme }}.min.css">
+    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/jqtree/jqtree.css">
+    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/styles/application.css">
+    <link rel="stylesheet" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/styles/styles.css">
 
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/jquery/jquery.min.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/jquery/jquery.min.js"></script>
     <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}styles/bootstrap/passwordstrength/jquery.passwordstrength.js">
     </script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/noty/jquery.noty.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/noty/layouts/topRight.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/noty/layouts/topCenter.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/noty/themes/default.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/jqtree/tree.jquery.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/bootbox/bootbox.min.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/bootbox/bootbox.locales.min.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/popper/popper.min.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/noty/jquery.noty.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/noty/layouts/topRight.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/noty/layouts/topCenter.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/noty/themes/default.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/jqtree/tree.jquery.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/bootbox/bootbox.min.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/bootbox/bootbox.locales.min.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/popper/popper.min.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/bootstrap/bootstrap.min.js"></script>
     <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}styles/bootstrap/bootstrap/js/bootstrap-typeahead.js"></script>
     <script
-        src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/bootstrap-datepicker/js/bootstrap-datepicker.js">
+        src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/bootstrap-datepicker/js/bootstrap-datepicker.js">
     </script>
 
     <link rel="stylesheet" href="{{ asset('css/zooplus.css') }}">
@@ -47,24 +47,24 @@
     @foreach(['de', 'es', 'ar', 'el', 'bg', 'ru', 'hr', 'hu', 'ko', 'pl', 'ro', 'sk', 'tr', 'uk', 'ca', 'nl', 'fi',
     'cs', 'it', 'fr', 'sv', 'sl', 'pt-BR', 'zh-CN', 'zh-TW'] as $lang)
     <script
-        src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/bootstrap-datepicker/locales/bootstrap-datepicker.{{ $lang }}.min.js">
+        src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/bootstrap-datepicker/locales/bootstrap-datepicker.{{ $lang }}.min.js">
     </script>
     @endforeach
 
     <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}styles/bootstrap/chosen/js/chosen.jquery.min.js"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/vendors/select2/js/select2.min.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/vendors/select2/js/select2.min.js"></script>
     <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}out/out.ViewFolder.php?action=webrootjs"></script>
-    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/styles/application.js"></script>
+    <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/styles/application.js"></script>
     <script src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}out/out.ViewFolder.php?action=js"></script>
     <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 
 
-    <link rel="icon" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/images/favicon.svg"
+    <link rel="icon" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/images/favicon.svg"
         type="image/svg+xml" />
     <link rel="apple-touch-icon" sizes="180x180"
-        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/images/apple-touch-icon.png">
+        href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/{{ $user->theme }}/images/apple-touch-icon.png">
     <title>Dashboard ZooPlus</title>
 
 </head>
