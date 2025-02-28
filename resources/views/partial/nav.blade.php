779404abc4e79c188e3ecf9e55a492967688af27
Krzysztof Stanek <ekstanek@gmail.com>
2025-03-01 00:35:37 +0100

    logo correction

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom fixed-top">
         <a class="navbar-brand" href="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}out/out.ViewFolder.php"><img id="navbar-logo"
-                src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}oviews/bootstrap4/images/seeddms-logo.svg"> <span
+                src="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}views/bootstrap4/images/seeddms-logo.svg"> <span
                 class="d-none d-md-inline-block ml-4">SeedDMS</span></a>
         <form action="{{ config('app.seeddms_url') }}{{ config('app.seeddms_url_additional') }}out/out.Search.php" class="form-inline ml-4 mr-auto"
             autocomplete="off"> <input type="hidden" name="folderid" value="1" /> <input type="hidden" name="navBar"
