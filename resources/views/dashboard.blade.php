52776174e18584f927dcdf2f4a2ef3e5a4539ab3
Krzysztof Stanek <ekstanek@gmail.com>
2025-03-12 22:24:44 +0100

    panel top5 countries off

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -56,61 +56,9 @@
                                 </div>
 
 
-                                <div class="row">
-                                    <div class="card card-success">
-                                        <div class="card-header">
-                                            <h3 class="card-title">Bar Chart</h3>
+                                
 
-                                            <div class="card-tools">
-                                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                                    <i class="fas fa-minus"></i>
-                                                </button>
-                                                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                                    <i class="fas fa-times"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="chart">
-                                                <div class="chartjs-size-monitor">
-                                                    <div class="chartjs-size-monitor-expand">
-                                                        <div class=""></div>
-                                                    </div>
-                                                    <div class="chartjs-size-monitor-shrink">
-                                                        <div class=""></div>
-                                                    </div>
-                                                </div>
-                                                <canvas id="barChart"
-                                                    style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 875px;"
-                                                    width="783" height="223" class="chartjs-render-monitor"></canvas>
-                                            </div>
-                                        </div>
-                                        <!-- /.card-body -->
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="card card-danger">
-                                        <div class="card-header">
-                                            <h3 class="card-title">TOP 5 Shipping Countries</h3>
-                                            <div class="card-tools">
-                                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                                    <i class="fas fa-minus"></i>
-                                                </button>
-                                                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                                    <i class="fas fa-times"></i>
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="chart">
-                                                <canvas id="pieChart"
-                                                    style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
+                                {{-- @include('panels/dashboard-top5-countries') --}}
 
                             </div>
 
